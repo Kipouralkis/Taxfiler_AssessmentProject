a67ae4deb8978c2388b23ac657cf2c3d20dd8b11
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2024-06-22 18:47:08 +0300

    Advice style adjustments

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
-// flashes.js
-
 document.addEventListener("DOMContentLoaded", function() {
+
+    // Flask message transitions
     var successFlash = document.querySelector(".flash-success");
 
     if (successFlash) {
@@ -12,4 +12,29 @@ document.addEventListener("DOMContentLoaded", function() {
             }, 1000); // remove element 1s after the transition occurs
         }, 2000);  // trigger transition after 2s of element on screen
     }
+
+
+    // Scroll to advice section
+    var adviceSection = document.getElementById("advice-section");
+    if (adviceSection) {
+        adviceSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Toggle advice border
+    function toggleAdviceBorder() {
+        adviceElement = document.querySelector('.advice');
+
+        if(window.innerWidth <= 768) {
+            adviceElement.classList.remove('gradient-border');
+        } else {
+            adviceElement.classList.add('gradient-border');
+        }
+    }
+
+    // Run on page load
+    window.addEventListener('load', toggleAdviceBorder);
+
+    // Run on resize
+    window.addEventListener('resize', toggleAdviceBorder);
+
 });
